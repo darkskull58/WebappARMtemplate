af28f95495ef9b922cae43b88e97fed7c03d1f4e
TANMAY BISWAS <tanmay.biswas-coep@bvucoep.edu.in>
2022-12-05 05:32:10 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,8 @@ provider "azurerm" {
     features {}
   # Configuration options
 }
+resource "random_uuid" "test" {
+}
 
 resource "azurerm_resource_group" "example" {
   name     = "demo"
@@ -15,7 +17,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "yashstorageaccount"
+  name                     = "yashstorageaccount-${random_uuid.test.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
